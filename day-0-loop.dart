e0ffac95c4dbd9d36a3569b2593e1dcfa22a2411
Syed Bipul Rahman <syedbipulrahman2@gmail.com>
2024-07-31 19:37:43 +0600

    loop added

---
+++ day-0-loop.dart
@@ -0,0 +1,28 @@
+void main() {
+//for , while , do while , foreach ......
+
+  // for (int i = 1; i <= 10; i++) {
+  //   print("I Love Your mithila ❤️");
+  // }
+
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+//while loop
+  // int topan = 11;
+
+  // while (topan <= 10) {
+  //   ++topan;
+  //   print("I Love Your mithila ❤️");
+  // }
+
+//do while loop
+  // do {
+  //   print("I Love Your mithila ❤️");
+  //   ++topan;
+  // } while (topan <= 10);
+
+
+  
+}
