865e61278ab20d4d3ab03354b5edba7642d303d6
Syed Bipul Rahman <syedbipulrahman2@gmail.com>
2024-07-31 19:16:52 +0600

    first commit

---
+++ day-0-control-flow.dart
@@ -0,0 +1,38 @@
+void main() {
+  // var x = 14;
+  // var y = 11;
+
+  // if (x < y) {
+  //   print("y is greater then x");
+  // } else {
+  //   print("x is greater than y");
+  // }
+
+// x,y,z x>y?? x>z??  <x>
+
+  // var x = 90, y = 6, z = 4;
+
+  // if (x > y && x > z) {
+  //   print("x is greatest number and it is: $x");
+  // } else if (y > x && y > z) {
+  //   print("y is greatest number and it is: $y");
+  // } else {
+  //   print("z is greatest number and it is: $z");
+  // }
+
+  var name = "payel";
+
+  //switch case
+  switch (name) {
+    case "topan":
+      print("my name is : $name");
+    case "mithila":
+      print("my name is : $name");
+    case "prosanto":
+      print("my name is : $name");
+    case "payel":
+      print("my name is : $name");
+    default:
+      print("no condition were matched");
+  }
+}
---
+++ day-0.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+main() {
+  
+  var names = ["topan roy", "juthi rani", "mithila roy"];
+  var ages = [12, 13, 50];
+  var age = [12.3, 13.22, 50.100];
+  // var x="vajng";
+  print(
+      "first : ${names.runtimeType}\n second: ${ages.runtimeType}\n third: ${age.runtimeType}");
+}
